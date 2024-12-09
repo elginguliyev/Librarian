bbbf6ac4262a259a9c1df223a81f262a4ffa4f24
Elgin <81240513+elginguliyev@users.noreply.github.com>
2024-12-09 16:18:28 +0400

    delete exceptions

--- src/main/java/com/example/controller/AuthController.java
+++ src/main/java/com/example/controller/AuthController.java
@@ -1,15 +1,12 @@
 package com.example.controller;
 
-import com.example.exceptions.MyExceptions;
 import com.example.request.LoginRequest;
 import com.example.security.MyTokenManager;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -20,22 +17,17 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
 
-    private final MyTokenManager jwtTokenProvider;
+    private final MyTokenManager tokenManager;
     private final AuthenticationManager authenticationManager;
 
     @PostMapping("login")
-    public ResponseEntity<String> login(@Valid
-                                        @RequestBody LoginRequest loginRequest,
-                                        BindingResult br) {
-        if (br.hasErrors()) {
-            throw new MyExceptions("Məlumatlar düzgün daxil edilməyib", br);
-        }
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
                         loginRequest.getPassword())
         );
         String username = authentication.getName();
-        String token = jwtTokenProvider.generateToken(username);
+        String token = tokenManager.generateToken(username);
         return ResponseEntity.ok(token);
     }
 }
--- src/main/java/com/example/request/LoginRequest.java
+++ src/main/java/com/example/request/LoginRequest.java
@@ -7,11 +7,9 @@ import lombok.Data;
 @Data
 public class LoginRequest {
 
-    @NotBlank(message = "Username boş ola bilməz")
-    @Size(min = 3, message = "Username ən azı 3 simvol olmalıdır")
+
     private String username;
-    @NotBlank(message = "Parol boş ola  bilməz")
-    @Size(min = 6, message = "Password ən azı 6 simvol olmalıdır")
+
     private String password;
 
 }
